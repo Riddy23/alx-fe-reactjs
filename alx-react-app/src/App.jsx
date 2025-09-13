d926e52f86987709a6820178e280f2480e30a2f5
Riddy23 <redemptionsuwali@gmail.com>
2025-09-13 14:52:08 +0000

    past

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,21 +1,18 @@
-
 import Header from './components/Header';
-import Footer from './components/Footer';
 import MainContent from './components/MainContent';
-import WelcomeMessage from './components/WelcomeMessage';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
-    return (
-        <div>
-            
-            < Header />
-            < MainContent />
-            <Footer />
-            <WelcomeMessage />
-                  </div>
-                    );
-                    }
+  return (
+      <div>
+            <Header />
+                  <MainContent />
+                        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+                              <Footer />
+                                  </div>
+                                    );
+                                    }
 
-                    export default App;
-
-
+                                    export default App;
+                                    
