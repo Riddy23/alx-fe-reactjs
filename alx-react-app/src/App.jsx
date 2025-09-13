6a43ff29c5123bf6c22b1879b9a5c37b87645fc2
Riddy23 <redemptionsuwali@gmail.com>
2025-09-13 14:28:03 +0000

    rander

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import WelcomeMessage from './components/WelcomeMessage';
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MainContent from './components/MainContent';
 
 function App() {
     return (
