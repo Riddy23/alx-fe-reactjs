f7825635cfe3b070e2ce4268807b129dc0c0901f
Riddy23 <redemptionsuwali@gmail.com>
2025-09-14 04:30:15 +0000

    fixed

--- alx-react-app-props/src/ProfilePage.jsx
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -1,8 +1,16 @@
-import UserInfo from './UserInfo';
+import { useContext } from 'react';
+import UserContext from '../UserContext';
 
-function ProfilePage() {
-  return <UserInfo />;
-  }
+function UserProfile() {
+  const userData = useContext(UserContext);
 
-  export default ProfilePage;
-  
+    return (
+        <div>
+              <p>Name: {userData.name}</p>
+                    <p>Email: {userData.email}</p>
+                        </div>
+                          );
+                          }
+
+                          export default UserProfile;
+                          
