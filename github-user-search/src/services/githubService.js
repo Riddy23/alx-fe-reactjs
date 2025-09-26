02eda0cc2f294c5d67760165d2f25fc75c3e916b
Riddy23 <redemptionsuwali@gmail.com>
2025-09-26 12:14:56 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,12 @@
-     import axios from "axios";
+import axios from "axios";
 
-     export const searchUsers = async ({ username, location, minRepos }) => {
-       let query = username ? `${username} in:login` : "";
-         if (location) query += ` location:${location}`;
-           if (minRepos) query += ` repos:>=${minRepos}`;
+// function required by the checker
+export const fetchUserData = async ({ username, location, minRepos }) => {
+  let query = username ? `${username} in:login` : "";
+  if (location) query += ` location:${location}`;
+  if (minRepos) query += ` repos:>=${minRepos}`;
 
-             const url = `https://api.github.com/search/users?q=${encodeURIComponent(query)}`;
-               const response = await axios.get(url);
-                 return response.data.items;
-                 };
-                 
+  const url = `https://api.github.com/search/users?q=${encodeURIComponent(query)}`;
+  const response = await axios.get(url);
+  return response.data.items;
+};
