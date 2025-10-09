a0d49b6097b0e5d7b8c53ad816871b487c5b0285
Riddy23 <redemptionsuwali@gmail.com>
2025-10-09 20:57:52 +0000

    gjur

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -7,7 +7,7 @@ const validationSchema = Yup.object({
       password: Yup.string().min(6, "Minimum 6 chars").required("Password is required"),
       });
 
-      export default function formikForm() {
+      export default function FormikForm() {
         return (
             <div className="max-w-md mx-auto mt-10 p-6 border rounded-md shadow-md bg-white">
                   <h2 className="text-2xl font-bold mb-4 text-center">Register with Formik</h2>
