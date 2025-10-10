546b4728eba37e7f365ce2ffd8e25d3c86e5a3ff
Riddy23 <redemptionsuwali@gmail.com>
2025-10-10 05:40:26 +0000

    task 2

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -5,6 +5,7 @@ import ProfileDetails from "./pages/ProfileDetails";
 import ProfileSettings from "./pages/ProfileSettings";
 import Login from "./pages/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
+import BlogPost from "./pages/BlogPost"; // ✅ Added import
 
 export default function App() {
   return (
@@ -12,25 +13,29 @@ export default function App() {
             <nav className="p-4 bg-gray-200 flex gap-4">
                     <Link to="/">Home</Link>
                             <Link to="/profile">Profile</Link>
-                                  </nav>
+                                    <Link to="/blog/1">Blog Example</Link> {/* ✅ Example link */}
+                                          </nav>
 
-                                        <Routes>
-                                                <Route path="/" element={<Home />} />
-                                                        <Route path="/login" element={<Login />} />
+                                                <Routes>
+                                                        <Route path="/" element={<Home />} />
+                                                                <Route path="/login" element={<Login />} />
 
-                                                                <Route
-                                                                          path="/profile/*"
-                                                                                    element={
-                                                                                                <ProtectedRoute>
-                                                                                                              <Profile />
-                                                                                                                          </ProtectedRoute>
-                                                                                                                                    }
-                                                                                                                                            >
-                                                                                                                                                      <Route path="details" element={<ProfileDetails />} />
-                                                                                                                                                                <Route path="settings" element={<ProfileSettings />} />
-                                                                                                                                                                        </Route>
-                                                                                                                                                                              </Routes>
-                                                                                                                                                                                  </Router>
-                                                                                                                                                                                    );
-                                                                                                                                                                                    }
-                                                                                                                                                                                    
+                                                                        {/* ✅ Dynamic blog route */}
+                                                                                <Route path="/blog/:id" element={<BlogPost />} />
+
+                                                                                        <Route
+                                                                                                  path="/profile/*"
+                                                                                                            element={
+                                                                                                                        <ProtectedRoute>
+                                                                                                                                      <Profile />
+                                                                                                                                                  </ProtectedRoute>
+                                                                                                                                                            }
+                                                                                                                                                                    >
+                                                                                                                                                                              <Route path="details" element={<ProfileDetails />} />
+                                                                                                                                                                                        <Route path="settings" element={<ProfileSettings />} />
+                                                                                                                                                                                                </Route>
+                                                                                                                                                                                                      </Routes>
+                                                                                                                                                                                                          </Router>
+                                                                                                                                                                                                            );
+                                                                                                                                                                                                            }
+                                                                                                                                                                                                            
